7c3abb519b83dd93898f2dbfcfa5291da91af995
XianghuWang-287 <wangxianghu1019@126.com>
2023-10-12 22:47:52 -0700

    Progress so far

---
+++ bin/clustering_eval.py
@@ -0,0 +1,68 @@
+import pandas as pd
+from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.metrics import adjusted_rand_score, adjusted_mutual_info_score
+import matplotlib.pyplot as plt
+
+
+
+cluster_results = pd.read_csv('/home/user/LAB_share/XianghuData/MS_Cluster_datasets/PXD023047_results/msculster_results/clustering/clusterinfo.tsv', sep='\t')  # Adjust file path and format accordingly
+
+
+database_results = pd.read_csv('./filtered.tsv', sep='\t')  # Adjust file path and format accordingly
+database_results['MzIDFileName'] = 'mzML/' + database_results['MzIDFileName']
+print("Unique values in cluster_results:")
+print("Filename:", cluster_results['#Filename'].unique())
+print("Scan:", cluster_results['#Scan'].unique())
+print("Charge:", cluster_results['#Charge'].unique())
+
+
+print("\nUnique values in database_results:")
+print("MzIDFileName:", database_results['MzIDFileName'].unique())
+print("ScanNumber:", database_results['ScanNumber'].unique())
+print("Charge:", database_results['Charge'].unique())
+
+unique_spectra_db = database_results[['MzIDFileName', 'ScanNumber']].drop_duplicates()
+
+# Filter cluster_results to only include spectra present in database results
+filtered_cluster_results = cluster_results.merge(unique_spectra_db,
+                                                  left_on=['#Filename', '#Scan'],
+                                                  right_on=['MzIDFileName', 'ScanNumber'],
+                                                  how='inner')
+
+# Merge the filtered clusters with the corresponding peptide identifications based on filename, scan number, and charge
+merged_data = pd.merge(filtered_cluster_results, database_results,
+                       left_on=['#Filename', '#Scan', '#Charge'],
+                       right_on=['MzIDFileName', 'ScanNumber', 'Charge'],
+                       how='inner')
+print(merged_data)
+# Assume 'true_labels' are the ground truth labels based on the database search
+true_labels = merged_data['Peptide']
+predicted_labels = merged_data['#ClusterIdx']
+
+
+ari = adjusted_rand_score(true_labels, predicted_labels)
+ami = adjusted_mutual_info_score(true_labels, predicted_labels)
+
+
+print('Adjusted Rand Index (ARI):', ari)
+print('Adjusted Mutual Information (AMI):', ami)
+
+cluster_purity = merged_data.groupby('#ClusterIdx')['Peptide'].apply(
+    lambda x: x.value_counts().max() / len(x))
+
+# Calculate the number of spectra in each cluster
+cluster_size = merged_data.groupby('#ClusterIdx').size()
+
+purity_by_cluster_size = pd.DataFrame({'ClusterSize': cluster_size, 'ClusterPurity': cluster_purity})
+
+# Group by cluster size and calculate average purity for each group
+average_purity_by_cluster_size = purity_by_cluster_size.groupby('ClusterSize')['ClusterPurity'].mean()
+
+# Plot average purity grouped by cluster size
+plt.figure(figsize=(10, 6))
+plt.plot(average_purity_by_cluster_size.index, average_purity_by_cluster_size.values, marker='o')
+plt.xlabel('Cluster Size (Number of Spectra)')
+plt.ylabel('Average Cluster Purity')
+plt.title('Average Cluster Purity by Cluster Size')
+plt.grid(True)
+plt.show()
---
+++ bin/ms1_scans.py
@@ -0,0 +1,118 @@
+import os
+from itertools import combinations
+from pyteomics import mzml
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pymzml
+from  xml.etree.ElementTree import ParseError
+
+# def get_ms1_data(mzml_file):
+#     ms1_data = []
+#     with mzml.read(mzml_file) as reader:
+#         for spectrum in reader:
+#             if spectrum['ms level'] == 2:
+#                 mass = float(spectrum['precursorList']['precursor']['selectedIonList']['cvParam'].get('value', 0))
+#                 rt = float(spectrum['scanList']['scan'][0]['scan start time'])
+#                 scan_number = int(spectrum['id'].split('scan=')[1])
+#                 ms1_data.append((mass, rt,scan_number))
+#     return ms1_data
+
+def get_ms1_data(mzml_file):
+    ms1_data = []
+
+    run = pymzml.run.Reader(mzml_file, build_index_from_scratch=False)
+
+
+
+    try:
+        for idx, spectrum in enumerate(run):
+            if spectrum.ms_level == 2:
+                precursors = spectrum.selected_precursors
+                if len(precursors) != 1:
+                    exception = NotImplementedError(
+                        "Expected one precursors but got {} while checking index {}.".format(len(precursors),
+                                                                                             spectrum.index))
+                    print(exception)
+                    print(mzml_file)
+                    continue
+                rt = float(spectrum.scan_time_in_minutes()) * 60
+                mass =precursors[0]['mz']
+                scan_number = int(spectrum['id'])
+                ms1_data.append((mass, rt, scan_number))
+    except Exception as e:
+            print(e)
+
+    return ms1_data
+
+def are_matching_spectra(filename, scan1, scan2,matching_pairs_set):
+    return (filename, scan1, scan2) in matching_pairs_set
+
+def calculate_cluster_purity(cluster, matching_pairs_set):
+    total_spectra = len(cluster)
+    if total_spectra == 0:
+        return 0.0
+
+    max_matching_fraction = 0.0
+
+    for index, spec1 in cluster.iterrows():
+        matching_count = 0
+        for _, spec2 in cluster.iterrows():
+            if spec1['#Filename'] != spec2['#Filename']:
+                continue
+
+            if are_matching_spectra(spec1['#Filename'], spec1['#Scan'], spec2['#Scan'], matching_pairs_set):
+                print('yes')
+                matching_count += 1
+
+        matching_fraction = matching_count / total_spectra
+
+        if matching_fraction > max_matching_fraction:
+            max_matching_fraction = matching_fraction
+
+    return max_matching_fraction
+
+
+
+
+def compare_scans(scan1, scan2, mass_tolerance=0.01, rt_tolerance=30):
+    mass_diff = abs(scan1[0] - scan2[0])
+    rt_diff = abs(scan1[1] - scan2[1])
+    return mass_diff <= mass_tolerance and rt_diff <= rt_tolerance
+
+
+if __name__ == "__main__":
+    folder_path = '/home/user/LAB_share/XianghuData/MS_Cluster_datasets/PXD023047_convert/mzML'
+
+    cluster_results = pd.read_csv('/home/user/LAB_share/XianghuData/MS_Cluster_datasets/PXD023047_results/msculster_results/clustering/clusterinfo.tsv',sep='\t')  # Adjust file path and format accordingly
+    matching_pairs_all_files = []
+
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.mzML'):
+            mzml_file = os.path.join(folder_path, filename)
+            ms1_data = get_ms1_data(mzml_file)
+
+            matching_pairs = []
+
+            for scan1, scan2 in combinations(ms1_data, 2):
+                if compare_scans(scan1, scan2):
+                    matching_pairs.append((scan1[2], scan2[2]))
+
+            matching_pairs_all_files.extend([(filename, pair[0], pair[1]) for pair in matching_pairs])
+
+    matching_pairs_set = {(item[0], item[1], item[2]) for item in matching_pairs_all_files}
+
+    cluster_purity = cluster_results.groupby('#ClusterIdx').apply(lambda x: calculate_cluster_purity(x, matching_pairs_set))
+
+    cluster_size = cluster_results.groupby('#ClusterIdx').size()
+
+    average_purity_by_cluster_size = cluster_purity.groupby(cluster_size).mean()
+
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(average_purity_by_cluster_size.index, average_purity_by_cluster_size.values, alpha=0.5)
+    plt.xlabel('Cluster Size (Number of Spectra)')
+    plt.ylabel('Average Cluster Purity')
+    plt.title('Average Cluster Purity by Cluster Size')
+    plt.grid(True)
+    plt.show()
